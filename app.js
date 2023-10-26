d9bfbc023c755a2412bcf08239119aee218859c0
Arun Kumar <aruncode49@gmail.com>
2023-10-26 23:02:36 +0530

    setting up cookie options

--- app.js
+++ app.js
@@ -21,6 +21,11 @@ const sessionOption = {
   secret: "ADFI24JDAF@4#%KAD",
   resave: false,
   saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
 };
 
 // middleware
