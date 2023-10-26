0f2183ed65d15c87c4ab62744790f4a7b4fd7c03
Arun Kumar <aruncode49@gmail.com>
2023-10-26 16:56:41 +0530

    storing info in session and use that info.

--- classroom/test.js
+++ classroom/test.js
@@ -4,27 +4,35 @@ const expressSession = require("express-session");
 const app = express();
 const PORT = 3000;
 
+// session options
+const sessionOptions = {
+  secret: "ADAgjadii343lalakd",
+  resave: false,
+  saveUninitialized: true,
+};
+
 // use express session
-app.use(
-  expressSession({
-    secret: "ADAgjadii343lalakd",
-    resave: false,
-    saveUninitialized: true,
-  })
-);
-
-app.get("/test-session", (req, res) => {
-  if (req.session.count) {
-    req.session.count++;
-  } else {
-    req.session.count = 1;
-  }
-
-  res.send(`You visit this website ${req.session.count} time`);
-});
+app.use(expressSession(sessionOptions));
 
 app.get("/", (req, res) => {
-  res.end("Hello this is the test file.");
+  res.send(`Hello this is the test file. Welcome ${req.session.name}`);
 });
+
+//   storing info in session and use that info.
+app.get("/register", (req, res) => {
+  const { name = "anonymous" } = req.query;
+  req.session.name = name;
+  res.redirect("/");
+});
+
+// app.get("/test-session", (req, res) => {
+//   if (req.session.count) {
+//     req.session.count++;
+//   } else {
+//     req.session.count = 1;
+//   }
+
+//   res.send(`You visit this website ${req.session.count} time`);
+// });
 
 app.listen(PORT, () => console.log("Server running on port 3000"));
