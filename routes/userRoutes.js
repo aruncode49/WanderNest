00b664c6353b5c922e9e121faf5738247a4cb32f
Arun Kumar <aruncode49@gmail.com>
2023-10-27 23:49:58 +0530

    registerd user with handling all type errors

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,27 @@
 const express = require("express");
+const wrapAsync = require("../utils/wrapAsync");
+const User = require("../models/userModel");
 const router = express.Router();
 
 router.get("/signup", (req, res) => {
   res.render("users/signup.ejs");
 });
+
+router.post(
+  "/signup",
+  wrapAsync(async (req, res) => {
+    try {
+      const { username, email, password } = req.body;
+      let newUser = new User({ username, email });
+      const registeredUser = await User.register(newUser, password);
+      console.log(registeredUser);
+      req.flash("success", "User created successfully");
+      res.redirect("/listings");
+    } catch (error) {
+      req.flash("error", error.message);
+      res.redirect("/signup");
+    }
+  })
+);
 
 module.exports = router;
