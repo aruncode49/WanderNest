9325d0a41116f1ec2be52c7647dfd7b1bc9e8b6b
Arun Kumar <aruncode49@gmail.com>
2023-11-05 23:36:51 +0530

    install dotenv and add cloud keys in env file

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV != "Production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
